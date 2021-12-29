93fe0cfb948d3ac8fec4e416e8c8888f914f4933
iazadur <iamazadur@gmail.com>
2021-12-29 20:50:04 +0600

    add reducer

---
+++ src/redux/actions/BookActions.js
@@ -0,0 +1,12 @@
+export function addtoReadingList(payload) {
+    return {
+        type: "ADD_TO_READING_LIST",
+        payload
+    }
+}
+export function removeFromReadingList(payload) {
+    return {
+        type: "REMOVE_FROM_READING_LIST",
+        payload
+    }
+}
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from "redux";
+import { bookReducer } from "./reducers/bookReducer";
+
+const rootReducer = combineReducers({
+    books: bookReducer,
+})
+
+const store = createStore(rootReducer);
+export default store
